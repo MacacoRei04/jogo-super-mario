8dbeb0067bb434295188c4f809a9d8085ad208a7
MacacoRei04 <163008169+MacacoRei04@users.noreply.github.com>
2024-09-11 11:13:48 -0300

    .

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ const pipe = document.querySelector(".pipe");
 const startbutton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 
+audioStart = new Audio("./sound/audio_theme.mp3");
+const gameOverSound = new Audio ("./sound/audio_gameover.mp3");
+
 let gameStarted = false;
 
 const startGame = () => {
-gameStarted = true;
+    gameStarted = true;
+    audioStart.play();
+}
 
 pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
@@ -14,19 +19,18 @@ startbutton.style.display = "none";
 mario.style.copacity = "1";
 pipe.style.copacity = "1";
 clouds.style.copacity = "1";
-
 }
 
-
 const jump = () => {
+    if(gameStarted) {
     mario.classList.add("jump");
 
     setTimeout (() => {
     mario.classList.remove("jump");
-    },500);
+    }
+    ,500);
 }
-
-
+}
 
 const loop = setInterval (() => {; 
 const pipePosition = pipe.offseftleft;
